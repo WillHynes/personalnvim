efdf3df8ebd13162f83eaad73cdb3a4815c9a52d
Will Hynes <williamhynes@gmail.com>
2023-11-26 22:41:22 +0000

    combine commits, plus add noice

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,34 +1,48 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-  -- Navigation
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-  },
-  'nvim-tree/nvim-tree.lua',
-  'stevearc/oil.nvim',
-  'ThePrimeagen/harpoon',
+	-- Navigation
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	"nvim-tree/nvim-tree.lua",
+	"stevearc/oil.nvim",
+	"ThePrimeagen/harpoon",
 
-  -- Aesthetic plugins
-  'rebelot/kanagawa.nvim',
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
-  'nvim-tree/nvim-web-devicons',
-  'nvim-lualine/lualine.nvim',
+	-- Aesthetic plugins
+	"rebelot/kanagawa.nvim",
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	"nvim-tree/nvim-web-devicons",
+	"nvim-lualine/lualine.nvim",
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 
+<<<<<<< Updated upstream
   -- QOL plugins
   {
     'romgrk/barbar.nvim',
@@ -48,34 +62,54 @@ local plugins = {
       vim.o.timeoutlen = 300
     end,
   },
+=======
+	-- QOL plugins
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+	},
+	{ "nvim-focus/focus.nvim",    version = "*" },
+	{ "akinsho/toggleterm.nvim",  version = "*",   config = true },
+	"David-Kunz/gen.nvim",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+	},
+>>>>>>> Stashed changes
 
-  -- Autocompletion / LSP plugins - this includes highlighting etc
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      require("core.plugin_config.null-ls") -- require your null-ls config here (example below)
-    end,
-  },
-  'nvimtools/none-ls.nvim',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  'rafamadriz/friendly-snippets',
-  'm4xshen/autoclose.nvim',
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
-  'nvim-treesitter/nvim-treesitter',
-  {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+	-- Autocompletion / LSP plugins - this includes highlighting etc
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("core.plugin_config.null-ls") -- require your null-ls config here (example below)
+		end,
+	},
+	"nvimtools/none-ls.nvim",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+	"m4xshen/autoclose.nvim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"nvim-treesitter/nvim-treesitter",
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 }
 
 local opts = {}
